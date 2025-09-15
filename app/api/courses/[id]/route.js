e159a1e1cc56b6f9c2333dcb2e4dfda40f7ff877
Shaharia hasan <hshaharia064@gmail.com>
2025-09-15 22:19:37 +0530

    created api to edit courses

--- app/api/courses/[id]/route.js
+++ app/api/courses/[id]/route.js
@@ -40,3 +40,52 @@ export async function DELETE(request, {params}){
 
     }
 }
+
+
+
+
+export async function PUT(request, {params}){
+    const {id} = params
+
+    try{
+        const {name, description, teacher_id} = await request.json()
+        const [result] = await pool.execute('UPDATE courses SET id=? name=?, description=?, teacher_id=? WHERE id=?', [id, name, description, teacher_id, id])
+
+        if(result.affectedRows === 0){
+            return NextResponse.json(
+                {message : 'No courses was found'},
+                {status : 404}
+            )
+        }
+
+
+        return NextResponse.json(
+            {id, name, description, teacher_id},
+            {status : 200}
+        )
+
+
+
+
+
+
+    }catch(err){
+        console.log('something went wrong while updating courses')
+                    if(  err && err.errno === 1453){
+                        return NextResponse.json(
+                            {message : 'courses table is connected to another table'},
+                            {status : 500}
+                        )
+                    }
+
+
+        return NextResponse.json(
+            {message : 'Courses was not updated'},
+            {status : 500}
+        )
+
+
+
+
+    }
+}
