a31eac7d77fedd983d0032ee06fecb20d1c0f60c
Shaharia hasan <hshaharia064@gmail.com>
2025-09-15 22:03:18 +0530

    created api to edit teachers

--- app/api/teachers/[id]/route.js
+++ app/api/teachers/[id]/route.js
@@ -47,3 +47,41 @@ export async function DELETE(request, {params}){
 
 
 }
+
+
+
+
+export  async function PUT(request, {params}){
+    const {id} = params
+    
+    try{
+        const body = await request.json()
+        const {name, email} = body 
+        const [result] = await pool.execute('UPDATE teachers SET name=?, email=? WHERE id=?', [name, email, id])
+        if(result.affectedRows ===0){
+          return  NextResponse.json(
+                {message : 'Teacher not found'},
+                {status : 404}
+            )
+        }
+
+
+        return NextResponse.json(
+            {id, name, email},
+            {status : 200}
+        )
+
+    }catch(err){
+        console.log('error occured while updating the teacher')
+       return NextResponse.json(
+            {message : 'error occured updating teacher'},
+            {status : 500}
+        )
+
+
+    }
+
+
+
+
+}
